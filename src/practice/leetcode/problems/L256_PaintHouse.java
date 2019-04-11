2a05f69e37982015fc2e0ce96f643d51b955cd34
Anvay P <newton.avp@gmail.com>
2019-04-11 05:54:14 +0530

    L65 Valid Numbers.

--- src/practice/leetcode/problems/L256_PaintHouse.java
+++ src/practice/leetcode/problems/L256_PaintHouse.java
@@ -10,6 +10,20 @@ package practice.leetcode.problems;
  * with color green, and so on... Find the minimum cost to paint all houses.
  */
 public class L256_PaintHouse {
+    static class Solution {
+        int minCost(int costs[][]) {
+            if (costs == null || costs.length == 0) {
+                return 0;
+            }
+            for (int i = 1; i < costs.length; i++) {
+                costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+                costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+                costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+            }
+            int m = costs.length - 1;
+            return Math.min(Math.min(costs[m][0], costs[m][1]), costs[m][2]);
+        }
+    }
     public static void main(String[] args) {
         int[][] costs = {
                 {17, 2, 17},
@@ -22,17 +36,4 @@ public class L256_PaintHouse {
     }
 }
 
-class Solution {
-    int minCost(int costs[][]) {
-        if (costs == null || costs.length == 0) {
-            return 0;
-        }
-        for (int i = 1; i < costs.length; i++) {
-            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
-            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
-            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
-        }
-        int m = costs.length - 1;
-        return Math.min(Math.min(costs[m][0], costs[m][1]), costs[m][2]);
-    }
-}
+
